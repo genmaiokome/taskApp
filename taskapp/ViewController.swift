eaea82f31434a810dcd3df69a547e9fe7c251029
ryosuke.watanabe <genmaiokomey@yahoo.co.jp>
2020-11-11 16:08:26 +0900

    画像のコピーを作成して修正した。

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
+    
+    //データの数(＝セルの数)を返すメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
 
+    //各セルの内容を返すメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //再利用可能な　cell を取得する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+    //各セルを選択した時に実行されるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    //セルが削除可能なことを伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    
+    //Delete　ボタンが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView,commit editingStyle: UITableViewCell.EditingStyle, forRowAtindexPath: IndexPath) {
+        
+    }
 
 }
 
